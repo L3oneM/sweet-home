b42c6093229501185a905be306e36962c188dc9d
L3oneMclear <apoWebDevAcc@gmail.com>
2020-01-10 22:46:56 +0200

    more test for heroku

--- server.js
+++ server.js
@@ -19,11 +19,11 @@ app.use(cors())
 if(process.env.NODE_ENV === 'production') {
   app.use(compression);
   app.use(enforce.HTTPS({ trustProtoHeader: true }));
-  app.use(express.static(path.join(__dirname, 'client/build')))
+  app.use(express.static(path.join(__dirname, 'client/build')));
 
   app.get('*', function(req, res) {
-    res.sendFile(path.join(__dirname, 'client/build', 'index.html'))
-  })
+    res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+  });
 }
 
 app.listen(port, error => {
