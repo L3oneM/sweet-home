56d282bda47caa0247948ae14fd2e2cb8e0fe8d2
L3oneMclear <apoWebDevAcc@gmail.com>
2020-01-10 23:09:16 +0200

    more more test

---
+++ client/build/precache-manifest.ac4bef44e7cfe118c5cce65647ace635.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c833154c42d965eab895cb7b1008163",
+    "url": "/index.html"
+  },
+  {
+    "revision": "105bc5370fdc67295b0d",
+    "url": "/static/css/main.7fc9845b.chunk.css"
+  },
+  {
+    "revision": "3b86a684d9655622e775",
+    "url": "/static/js/2.e860fcd6.chunk.js"
+  },
+  {
+    "revision": "bece73c9988dcb614abc759d33b51b13",
+    "url": "/static/js/2.e860fcd6.chunk.js.LICENSE"
+  },
+  {
+    "revision": "105bc5370fdc67295b0d",
+    "url": "/static/js/main.d71ef8af.chunk.js"
+  },
+  {
+    "revision": "63fe1afd39780fc4e182",
+    "url": "/static/js/runtime-main.5fc1587c.js"
+  },
+  {
+    "revision": "e5600323c5ba8c68aaad8257fe4a3c22",
+    "url": "/static/media/Florsn01.e5600323.ttf"
+  },
+  {
+    "revision": "d73ea391c9b51dc44f2b5db707ab6950",
+    "url": "/static/media/Florsn33.d73ea391.ttf"
+  },
+  {
+    "revision": "059b8af5118536ffa1bb2978b9a52ad6",
+    "url": "/static/media/Florsn37.059b8af5.ttf"
+  },
+  {
+    "revision": "379265992e7e6b55712df73e4a07f8fe",
+    "url": "/static/media/homePageBack.37926599.png"
+  },
+  {
+    "revision": "70586c5ca4aa680fbe053f68956b95bb",
+    "url": "/static/media/logo2.70586c5c.svg"
+  }
+]);
